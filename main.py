34fc95d9ffd658f5b1b028656c259dc0f6e74d85
muhammadzaki693 <75458189+muhammadzaki693@users.noreply.github.com>
2022-10-02 10:02:47 +0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import zack
-import rich
-import json
-from zack.types import Point2d
-from zack.func import Text
-
-Text.table(["idk","idk"],["idk","idk"])
-Text.status("doing","task complete","done", ["task1","task2"])
